91587d23315841707fe927c503f6860108033ecb
Aykut Asil <aykuttasil@gmail.com>
2019-06-21 16:05:34 +0300

    hashset

---
+++ hashmap-hashset-hashtable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hashmap_hashset_hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new HashSet<String>();
+            for (int i = 0; i < 10; i++)
+            {
+                a.Add("a");
+            }
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+                /* Output
+                   a
+                 */
+            }
+        }
+    }
+}
