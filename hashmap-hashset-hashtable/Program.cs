c4477e90f3fd263bbb07d8f9b4a2004fb71072e8
Aykut Asil <aykuttasil@gmail.com>
2019-06-21 16:59:05 +0300

    hashtable sample

--- hashmap-hashset-hashtable/Program.cs
+++ hashmap-hashset-hashtable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace hashmap_hashset_hashtable
@@ -10,7 +11,7 @@ namespace hashmap_hashset_hashtable
 
             sampleHashSet();
             sampleHashMap();
-
+            sampleHashTable();
         }
 
         public static void sampleHashSet()
@@ -29,7 +30,7 @@ namespace hashmap_hashset_hashtable
             {
                 Console.WriteLine(item);
                 /* Output
-                   a
+                   a1
                  */
             }
             Console.WriteLine("-------------------");
@@ -50,10 +51,50 @@ namespace hashmap_hashset_hashtable
             foreach (var item in a)
             {
                 Console.WriteLine(item.Value);
-                /* Output
-                   a
-                 */
             }
+            /* Output
+                a0
+                a1
+                a2
+                a3
+                a4
+                a5
+                a6
+                a7
+                a8
+                a9
+            */
+            Console.WriteLine("-------------------");
+        }
+
+        public static void sampleHashTable()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("HashTable");
+
+            var a = new Hashtable();
+            for (int i = 0; i < 10; i++)
+            {
+                a.Add(i, "a" + i);
+                //a.Add(i, "a" + i); Runtime Error. Because it's must be once for same key.
+            }
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            /* Output
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+                System.Collections.DictionaryEntry
+             */
             Console.WriteLine("-------------------");
         }
     }
