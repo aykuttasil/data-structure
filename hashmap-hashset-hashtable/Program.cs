f2f476719ea8406529b522ce4378a7cca2d09a22
Aykut Asil <aykuttasil@gmail.com>
2019-06-21 16:41:58 +0300

    HashMap(Dictionary) sample

--- hashmap-hashset-hashtable/Program.cs
+++ hashmap-hashset-hashtable/Program.cs
@@ -7,10 +7,22 @@ namespace hashmap_hashset_hashtable
     {
         static void Main(string[] args)
         {
+
+            sampleHashSet();
+            sampleHashMap();
+
+        }
+
+        public static void sampleHashSet()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("HashSet");
+
             var a = new HashSet<String>();
             for (int i = 0; i < 10; i++)
             {
-                a.Add("a");
+                a.Add("a" + 1);
+                a.Add("a" + 1);
             }
 
             foreach (var item in a)
@@ -20,6 +32,29 @@ namespace hashmap_hashset_hashtable
                    a
                  */
             }
+            Console.WriteLine("-------------------");
+        }
+
+        public static void sampleHashMap()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("HashMap");
+
+            var a = new Dictionary<int, string>();
+            for (int i = 0; i < 10; i++)
+            {
+                a.Add(i, "a" + i);
+                // a.Add(i, "a" + i); Runtime Error. Because it's must be once for same key.
+            }
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Value);
+                /* Output
+                   a
+                 */
+            }
+            Console.WriteLine("-------------------");
         }
     }
 }
